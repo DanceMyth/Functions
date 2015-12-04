b33e480c6932b8485ff8556a07a6878b01f3c44e
Dan Smith <Dancemyth@icloud.com>
2015-12-04 16:37:46 +0000

    first commit

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func add(num1: Double, num2: Double) -> Double{
+    var result = num1 + num2
+    return result
+}
